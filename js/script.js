f160be61dd8a58ee3dff58f6a3201730ef920ebd
RauPro <00012119@uca.edu.sv>
2020-07-31 17:34:00 -0600

    Added: API

---
+++ js/script.js
@@ -0,0 +1,57 @@
+document.querySelector(".container-button").addEventListener('click', function() {
+	var inputValue = document.querySelector('.container-textinput').value;
+		var userInput = getUserInput();
+	searchGiphy( userInput );
+
+});
+document.querySelector('.container-textinput').addEventListener('keyup', function (e) {
+	if (e.which === 13) {
+ 		var userInput = getUserInput();
+ 		searchGiphy( userInput );
+    }
+});
+
+function getUserInput() {
+	var inputValue = document.querySelector('.container-textinput').value;
+
+	return inputValue;
+}
+function searchGiphy( searchQuery ) {
+	var url = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + searchQuery;
+
+	// AJAX Request
+	var GiphyAJAXCall = new XMLHttpRequest();
+	GiphyAJAXCall.open( 'GET', url );
+	GiphyAJAXCall.send();
+
+
+	GiphyAJAXCall.addEventListener('load', function( data ) {
+
+			var currentData = data.target.response;
+			pushToDOM(currentData);
+			console.log(currentData);
+		
+	});
+
+}
+function pushToDOM( response ) {
+	// turn response into real javascript object
+	response = JSON.parse( response );
+	// drill down to the data array
+	var images = response.data;
+
+	// find the container to hold this stuff in DOM
+	var container = document.querySelector('.js-container');
+	// clear it of old content since this function will be used on every search
+	// we want to reset the div
+	container.innerHTML = "";
+
+	// loop through data array and add IMG html
+	images.forEach(function(image){
+		// find img src
+		var src = image.images.fixed_height.url;
+
+		// concatenate a new IMG tag
+		container.innerHTML += "<img src='"+ src +"' class='container-image' />";
+	});
+}
